e1b6ed2c64ea440f45b43bcc9c0da51ab4374e39
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-08 12:05:12 -0300

    chore: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+      "es2022": true,
+      "node": true
+  },
+  extends: [
+      "eslint:recommended",
+      "standard-with-typescript",
+      "plugin:@typescript-eslint/recommended"
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '*.config.cjs'],
+  overrides: [
+      {
+          env: {
+              node: true
+          },
+          files: [
+              ".eslintrc.{js,cjs}"
+          ],
+          parserOptions: {
+              "sourceType": "script"
+          }
+      }
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+      ecmaVersion: "ES2022",
+      sourceType: "module",
+      project: "./tsconfig.json",
+      tsconfigRootDir: __dirname
+  },
+  plugins: [
+      "@typescript-eslint"
+  ],
+  rules: {
+  }
+}
