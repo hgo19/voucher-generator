96a738733f3a3f7c575818de19752cf032ee7e63
Hugo Leonardo <huleonardo19@gmail.com>
2023-08-08 16:30:10 -0300

    feat: implements VoucherGenerator to send an email with the right pdf

--- src/services/voucher-generator.ts
+++ src/services/voucher-generator.ts
@@ -1,3 +1,4 @@
+import { emailClient } from './helpers/nodemailer-transport'
 import { createPDF } from './helpers/pdf-generator'
 
 export interface UserData {
@@ -10,7 +11,14 @@ export interface UserData {
 }
 
 export class VoucherGenerator {
-  async execute (userData: UserData): Promise<void> {
+  async static execute (userData: UserData): Promise<void> {
     const base64Pdf = createPDF(userData)
+    await emailClient.sendMail({
+      from: process.env.EMAIL,
+      to: userData.email,
+      subject: 'Hotel Voucher',
+      text: 'The file containing the information is attached',
+      attachments: [{ path: base64Pdf }]
+    })
   }
 }
